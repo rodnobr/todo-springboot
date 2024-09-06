3460f69a786b413f611f5f4106e63f9698624157
Rodrigo Nobre <roinobre@gmail.com>
2024-09-06 03:23:36 -0400

    first commit

---
+++ src/main/java/br/com/rodnobr/todolist/user/UserController.java
@@ -0,0 +1,32 @@
+package br.com.rodnobr.todolist.user;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null) {
+            System.out.println("user already exists");
+            return ResponseEntity.status(400).body("User already exists");
+        }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashed);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(201).body(userCreated);
+    }
+
+
+}
